83b5da9c1d3c80bce0e7741623c674ed65e17711
jxxdxxexx <jasminedenriquez@gmail.com>
2025-06-18 22:03:28 +0800

    add projects

--- script.js
+++ script.js
@@ -134,3 +134,20 @@ feedbackForm.addEventListener('submit', function (e) {
   }, 300);
 
 });
+
+ function openModal(id) {
+    document.getElementById(id).style.display = 'flex';
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
+
+  // Optional: Close modal on outside click
+  window.addEventListener('click', function(e) {
+    document.querySelectorAll('.project-modal').forEach(modal => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
